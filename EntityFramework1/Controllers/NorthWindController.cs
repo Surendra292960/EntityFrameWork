f826a05e498a5e2944fe9e4f8b7483595fe511cc
Surendra292960 <surendrapalatm@outlook.com>
2018-10-26 15:35:59 +0630

    commited

---
+++ EntityFramework1/Controllers/NorthWindController.cs
@@ -0,0 +1,23 @@
+﻿using EntityFramework1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EntityFramework1.Controllers;
+
+namespace EntityFramework1.Controllers
+{
+    public class NorthWindController : Controller
+    {
+        NorthWindContext db = new NorthWindContext();
+
+        public ActionResult Index()
+        {
+            db.CategoriesTable.ToList();
+            db.ProductTable.ToList();
+
+            return View();
+        }
+    }
+}
---
+++ EntityFramework1/Models/NorthWindContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace EntityFramework1.Models
+{
+    public class NorthWindContext:DbContext
+    {
+        public NorthWindContext()
+            :base("name =SchoolDBConnectionString")
+        {
+
+        }
+
+        public DbSet<Category> CategoriesTable { get; set; }
+        public DbSet<Product> ProductTable { get; set; }
+    }
+
+}
